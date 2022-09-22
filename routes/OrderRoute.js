3ef7083d29dae4fad80af505f37ac0f6d36c5881
can7wysiwyg <paulkssa@gmail.com>
2022-09-22 16:15:02 +0200

    server code on showing orders

--- routes/OrderRoute.js
+++ routes/OrderRoute.js
@@ -1,43 +1,31 @@
 const OrderRoute = require("express").Router();
 const verify = require("../middleware/verify");
 const authAdmin = require("../middleware/authAdmin");
-const {Order, CartItem} = require("../models/order")
-const { errorHandler} = require("./errorHandler")
+const { Order, CartItem } = require("../models/order");
+const { errorHandler } = require("./errorHandler");
 const asyncHandler = require("express-async-handler");
 
 OrderRoute.post(
   "/cart/create_order",
   verify,
   asyncHandler(async (req, res) => {
+    // req.body.order = req.user.id
 
-    // req.body.order = req.user.id 
-
-    const {products, amount} = req.body
+    const { products, amount } = req.body;
     const order = new Order({
       products,
       amount,
-      user: req.user.id
+      user: req.user.id,
     });
-   await order.save((error, data) => {
-        if (error) {
-            return res.status(400).json({
-                error: errorHandler(error)
-            });
-        
-        } 
-        
-
-      
+    await order.save((error, data) => {
+      if (error) {
+        return res.status(400).json({
+          error: errorHandler(error),
+        });
       }
-      
+    });
 
-        )
-    
-        res.json({msg: "order succesfully created"});
-
-
-
-    
+    res.json({ msg: "order succesfully created" });
   })
 );
 
@@ -52,17 +40,77 @@ OrderRoute.get(
   })
 );
 
+
 OrderRoute.get(
-  "/cart/show_user_carts",
+  "/cart/show_not_processed",
   verify,
+  authAdmin,
   asyncHandler(async (req, res) => {
 
-await Order.find({user: req.user.id}).then(orders => res.json({orders}))
+    const not_processed = await Order.find({status: "Not processed"})
 
-
+    res.json({not_processed})
+   
 
 
   })
 );
+
+OrderRoute.get(
+  "/cart/show_processing",
+  verify,
+  authAdmin,
+  asyncHandler(async (req, res) => {
+
+    const processing = await Order.find({status: "Processing"})
+
+    res.json({processing})
+   
+  })
+);
+
+OrderRoute.get(
+  "/cart/show_delivered",
+  verify,
+  authAdmin,
+  asyncHandler(async (req, res) => {
+
+    const delivered = await Order.find({status: "Delivered"})
+
+    res.json({delivered})
+   
+  })
+);
+
+OrderRoute.get(
+  "/cart/show_cancelled",
+  verify,
+  authAdmin,
+  asyncHandler(async (req, res) => {
+
+    const cancelled = await Order.find({status: "Cancelled"})
+
+    res.json({cancelled})
+   
+  })
+);
+
+
+OrderRoute.get(
+  "/cart/show_user_carts",
+  verify,
+  asyncHandler(async (req, res) => {
+    await Order.find({ user: req.user.id }).then((orders) =>
+      res.json({ orders })
+    );
+  })
+);
+
+
+
+
+
+
+
 
 module.exports = OrderRoute;
